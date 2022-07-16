76418c8b9bdc46456348b5f86a14e9376f411f12
Linsey <linseyrimmer@hotmail.co.uk>
2022-07-16 23:28:20 +0100

    :lipstick: Style recipe list view page

--- front_mealplanner/src/AppHeaderContent/AppHeader.js
+++ front_mealplanner/src/AppHeaderContent/AppHeader.js
@@ -1,11 +1,12 @@
 import { useState, useEffect, useReducer } from 'react'; 
+import { Link } from "react-router-dom"; 
 import NavBar from './NavBar'; 
 import './AppHeader.css'; 
 
 export default function AppHeader() {
     return (
         <header className='app-header'>
-            <h1>Meal Planner App</h1>
+            <Link to="/"><h1>Meal Planner App</h1></Link>
             <NavBar />
         </header>
     )
--- front_mealplanner/src/RecipeList.js
+++ front_mealplanner/src/RecipeList.js
@@ -1,9 +1,21 @@
 import { useState, useEffect, useReducer } from 'react'; 
+import DummyData from './dummydata.json'; 
+import './RecipeList.css'; 
 
 export default function RecipeList() {
     return (
         <div>
-            <h1>Hello You</h1>
+            <div className='subheader'>
+                <h2>Recipes</h2>
+            </div>
+            
+            <ul className='recipe-list'>
+                {
+                    DummyData.map( p => {
+                        return <li key={p.name}><i class="fa-solid fa-utensils"></i> {p.name}</li>
+                    })
+                }
+            </ul>
         </div>
     )
 }
